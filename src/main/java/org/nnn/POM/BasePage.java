01403d77511e8935fa8a6a1bf0003af2dc41cdbd
Nikolay Nikolov <skilloeducation@abv.bg>
2025-05-22 15:50:02 +0300

    Adding Page factory locators and way of thinking

--- src/main/java/org/nnn/POM/BasePage.java
+++ src/main/java/org/nnn/POM/BasePage.java
@@ -1,6 +1,7 @@
 package org.nnn.POM;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -20,9 +21,29 @@ public class BasePage {
 
     public void navigateTo(String pageURLSUFIX) {
         String urlToBeLoaded = BASE_URL+pageURLSUFIX;
-
         driver.get(urlToBeLoaded);
         System.out.println("THE USER HAS NAVIGATED TO " + urlToBeLoaded);
     }
 
+     public void fillField(WebElement elm, String txt){
+        wait.until(ExpectedConditions.visibilityOf(elm));
+        elm.clear();
+        elm.sendKeys(txt);
+     }
+
+    public void clickOn(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+        System.out.println("The user has clicked on " + element);
+    }
+
+    private String locatorInfo(WebElement elm ){
+        String[] rawWebElmInfo = elm.toString().split("->");
+        String[] webElmInfo = rawWebElmInfo[1].split(":");
+        String locatorStrategy = webElmInfo[0];
+        String locatorExpression = webElmInfo[1];
+        String info = "LOCATOR STRATEGY BY : "+ locatorStrategy.toUpperCase() + " WITH LOCATOR EXPRESSION"+ locatorExpression;
+        return info;
+    }
 }
--- src/main/java/org/nnn/POM/RegistrationPage.java
+++ src/main/java/org/nnn/POM/RegistrationPage.java
@@ -1,4 +1,102 @@
 package org.nnn.POM;
 
-public class RegistrationPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class RegistrationPage extends BasePage {
+
+    public static final String REG_PAGE_SUFIX = "/users/register";
+    private static final String EXPECTED_REG_FORM_HEADER_TEXT = "Sign up";
+    private static final String EXPECTED_REG_MSG = "Successful register!";
+
+    //2.LOCATORS
+    //Registration form
+    @FindBy(xpath = "//input[contains(@name, \"username\")]") // = driver.findElement(By.xpath(USERNAME_REG_INPUT_FIELD_XPATH));
+    private WebElement regUsernameInputField;
+
+    @FindBy (xpath = "//input[contains(@type, \"email\")]")
+    private WebElement regEmailInputField;
+
+    @FindBy (xpath = "//input[contains(@formcontrolname, \"birthDate\")]")
+    private WebElement regBirthDateInputField;
+
+    @FindBy (id = "defaultRegisterFormPassword" )
+    private WebElement regPasswordInputField;
+
+    @FindBy (id = "defaultRegisterPhonePassword")
+    private WebElement regConfirmPasswordInputField;
+
+    @FindBy (xpath = "//textarea")
+    private WebElement publicInfoTextArea;
+
+    @FindBy (id = "sign-in-button" )
+    private WebElement registrationFormSubmitButton;
+
+
+    public RegistrationPage (WebDriver driver){
+        super(driver);
+        PageFactory.initElements(driver,this);
+    }
+
+    // Navigation to the page
+    public void navigateToRegistrationPageByURL(){
+        navigateTo(REG_PAGE_SUFIX);
+    }
+
+    //User Actions
+    //Populate data in input fields
+    public void provideUserName(){
+        String providedDemoUser = demoUsername();
+        fillField(regUsernameInputField,providedDemoUser);
+    };
+
+    public void provideEmail(){
+        fillField(regEmailInputField,randomValidEmail());
+    };
+
+    public void provideBDayInfo(){
+        fillField(regBirthDateInputField,"22022022");
+    };
+
+    public void providePass(){
+        fillField(regPasswordInputField,"22022022!A");
+    };
+
+    public void providePassConfirm(){
+        fillField(regConfirmPasswordInputField,"22022022!A");
+    };
+
+    public void providePublicInfo(){
+        fillField(publicInfoTextArea,"Public profile");
+    };
+
+    public void clickOnSubmitBtn(){
+        clickOn(registrationFormSubmitButton);
+    }
+
+
+    //Support methods for reg page
+    public String getCurrentTime() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HHmmss");
+        String formattedDateTime = now.format(formatter);
+        return formattedDateTime;
+    }
+
+
+    //Support utils for test data gen
+    public String demoUsername() {
+        String username = "Demo" + getCurrentTime();
+        return username;
+    }
+
+    public String randomValidEmail() {
+        String email = "demo" + getCurrentTime() + "@gmail.com";
+        return email;
+    }
 }
